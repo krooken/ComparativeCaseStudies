2c27d8d98ca44614d1e2ba841a1517ad6ed3298e
Jonas Krook <jonas.krook@zenuity.com>
2019-03-13 10:51:42 +0100

    Cleaning up and adding comments

--- AutonomousDriving/TuLiP_AutonomousDriving.py
+++ AutonomousDriving/TuLiP_AutonomousDriving.py
@@ -5,13 +5,11 @@ from __future__ import print_function
 import logging
 import pickle
 
-import numpy as np
-from tulip import spec, synth, hybrid
+from tulip import spec, synth
 
 
 # The road and vehicles parameters and values
-width_road = 1
-Safe_Distance = 2
+safe_distance = 2
 road_length = 30
 max_speed = 2
 
@@ -31,32 +29,55 @@ env_init = {'pos_env = 10', 'vlc_env = 0', 'lane_env = "right"'}
 
 env_safe =  {
 
+    # If the two vehicles are in different lanes, no safety distance is required.
+    # If the vehicles are in the same lane, three cases is taken care of:
+        # 1. The other vehicle is farther from the road ends than the safety distance.
+        #    In this case this vehicle needs to be positioned before or after the safety distance.
+        # 2. The other vehicle is close to the road start.
+        #    In this case the rear safety distance wraps around and some positions at the road end cannot be
+        #    occupied by this vehicle.
+        # 3. The other vehicle is close to the road end.
+        #    In this case the front safety distance wraps around and some positions at the road start cannot
+        #    be occupied by this vehicle.
     ("lane_ego != lane_env" +
      "| (pos_ego > {l}-1 && pos_ego < {L}-{l} && (pos_env - pos_ego > {l} | pos_env - pos_ego < -{l}))" +
      "| (pos_ego < {l} && pos_env > pos_ego+{l} && pos_env < pos_ego+{L}-{l})" +
      "| (pos_ego > {L}-{l}-1 && pos_env > pos_ego-{L}+{l} && pos_env+{l} < pos_ego)")
-        .format(l=Safe_Distance, L=road_length),
+        .format(l=safe_distance, L=road_length),
 
+    # Similar to previous comment, but this time it is made sure that the next position does not violate
+    # the safety distance.
     ("lane_ego != lane_env' " +
      "| (pos_ego > {l}-1 && pos_ego < {L}-{l} && (pos_env' - pos_ego > {l} | pos_env' - pos_ego < -{l}))" +
      "| (pos_ego < {l} && pos_env' > pos_ego+{l} && pos_env' < pos_ego+{L}-{l})" +
      "| (pos_ego > {L}-{l}-1 && pos_env' > pos_ego-{L}+{l} && pos_env'+{l} < pos_ego)")
-        .format(l=Safe_Distance, L=road_length),
+        .format(l=safe_distance, L=road_length),
     
+    # If the vehicle changes lanes, from right to left, then the left indicator should be active.
+    # Lane changes can only be performed if the vehicle is moving.
     ('''(lane_env = "right" && lane_env' = "left")  ->''' +
      '''(ind_env = "left"  && ind_env' = "none" && vlc_env > 0)'''),
     
+    # Same as previous, but left to right.
     ('''(lane_env = "left" && lane_env' = "right")  ->''' +
      '''(ind_env = "right" && ind_env' = "none" && vlc_env > 0)'''),
     
+    # When driving in the left lane it is not allowed to have the left indicator active.
     ('''lane_env = "left" -> (ind_env = "right"  |  ind_env = "none")'''),
-     
+    
+    # Same as previous, but right lane and right indicator.
     ('''lane_env = "right" -> (ind_env = "left"  |  ind_env= "none")'''),
-     
+    
+    # Modulus 30 implementation.
+    # If the current position plus the current velocity is less than 30, the speed can be added to
+    # the current position to get the next position.
     ('''((pos_env = {L}-2 && (vlc_env = 0 | vlc_env = 1)) | (0 <= pos_env && pos_env<= {L}-3) | (pos_env = {L}-1 && vlc_env = 0) ) ->''' +
      '''pos_env' = pos_env + vlc_env''')
         .format(L=road_length),
-     
+    
+    # Modulus 30 implementation.
+    # If the current position plus the current velocity is 30 or more, then the road length
+    # is subtracted from the position, and then the velocity is added.
     ('''(pos_env = {L}-2 && vlc_env = 2 | pos_env >= {L}-1 && vlc_env > 0) ->''' +
      '''pos_env' = pos_env-{L} + vlc_env''')
         .format(L=road_length),
@@ -87,6 +108,8 @@ sys_init = {'pos_ego = 0', 'vlc_ego = 0', 'lane_ego = "right"'}
 
 sys_prog = {
 
+     # An autonomous vehicle needs to be able to drive.
+     # Force it to run infinitely many laps.
      'vlc_ego > 0',
      'pos_ego = 0'
 
@@ -94,17 +117,18 @@ sys_prog = {
 
 sys_safe = {
 
+    # See specification for env_vehicle for explanations.
     ("lane_ego != lane_env" +
      "| (pos_env > 0 && pos_env < {L}-{l} && (pos_env - pos_ego > {l} | pos_env - pos_ego < -{l}))" +
      "| (pos_env < {l} && pos_ego > pos_env+{l} && pos_ego < pos_env+{L}-{l})" +
      "| (pos_env > {L}-{l}-1 && pos_ego > pos_env-{L}+{l} && pos_ego+{l} < pos_env)")
-        .format(l=Safe_Distance, L=road_length),
+        .format(l=safe_distance, L=road_length),
 
     ("lane_ego' != lane_env" +
      "| (pos_env > 0 && pos_env < {L}-{l} && (pos_env - pos_ego' > {l} | pos_env - pos_ego' < -{l}))" +
      "| (pos_env < {l} && pos_ego' > pos_env+{l} && pos_ego' < pos_env+{L}-{l})" +
      "| (pos_env > {L}-{l}-1 && pos_ego' > pos_env-{L}+{l} && pos_ego'+{l} < pos_env)")
-        .format(l=Safe_Distance, L=road_length),
+        .format(l=safe_distance, L=road_length),
    
     ('''(lane_ego = "right" && lane_ego' = "left") ->''' +
      '''(ind_ego = "left"  &&  ind_ego' = "none" && vlc_ego > 0)'''),
